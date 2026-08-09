2e132839a2e2c1aec968153afcb58cba2d153d26
dhirajmygithub <dhirajbarnwal24@gmail.com>
2026-08-09 08:59:26 +0530

    first commit

---
+++ RGCreation.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1-test"
+  location = "Central India"
+}
+resource "azurerm_storage_account" "saTest" {
+  name                     = "sadhirajtest"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
